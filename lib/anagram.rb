cb77e35e59338a92fc77231b111d0fec3229ca21
Gerardo Manzo <programmingmanzo@gmail.com>
2019-04-08 19:40:25 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram 
+  
+  attr_accessor :word 
+  
+  def initialize(word)
+    @word = word 
+  end
+  
+  def match(word_array)
+    word_array.select do |word|
+      (@word.split("").sort) == (word.split("").sort)
+      #splits the word and then sorts it in alphabetical order, placing the first character in the front. This compares the word that's being iterated against the instance variable. 
+      #binding.pry 
+    end 
+  end 
+end
